4e25e8956cc67e017a24ba0569aa3ab8b00e7087
mayank <my321532@dal.ca>
2022-03-10 20:55:46 -0400

    Adding train dataset and shell to load dataset

--- Research/DataAnalysis.py
+++ Research/DataAnalysis.py
@@ -1,18 +1,24 @@
 import gzip
 import numpy as np
-
+from PIL import Image as im
 
 class LoadData:
-    train_data = None
 
     def __init__(self):
         self
 
     def Load_Data():
         #change patch as per your file location
-        train_data = gzip.open('/Users/lib-user/Library/CloudStorage/OneDrive-DalhousieUniversity/Winter 2022/Machine Learning/featuresleuth/MNISTDataset/MNIST_train_data.gz', 'r')
+        file_reader = gzip.open('/Users/lib-user/Library/CloudStorage/OneDrive-DalhousieUniversity/Winter 2022/Machine Learning/featuresleuth/MNISTDataset/MNIST_train_data.gz', 'r')
+        file_reader.read(16)
+        buf = file_reader.read(28 * 28 * 1)
+        train_data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
         print('demo')
         return train_data
+
+    def PrintImage(data):
+        image = im.fromarray(np.reshape(data,(28,28)))
+        image.show()
 
     def __call__(self, ):
         return self.Load_Data()
--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from Research.DataAnalysis import LoadData
 
 if __name__ == '__main__':
     train_data = LoadData.Load_Data()
+    LoadData.PrintImage(train_data)
