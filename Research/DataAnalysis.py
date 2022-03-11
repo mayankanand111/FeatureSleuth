fb71fafd77c451e0700ecb209db015a3d197b1d8
rohini.chandrala <rohini.chandrala@seamless.se>
2022-03-11 18:17:53 -0400

    Added code for loading train label file

--- Research/DataAnalysis.py
+++ Research/DataAnalysis.py
@@ -2,23 +2,36 @@ import gzip
 import numpy as np
 from PIL import Image as im
 
+
 class LoadData:
+    # change patch as per your file location
+    file_path = '/Users/rohinichandrala/Documents/Personal_Docs/Dalhousie/ML/Project/featuresleuth/MNISTDataset/MNIST_train_data.gz'
 
     def __init__(self):
         self
 
     def Load_Data():
-        #change patch as per your file location
-        file_reader = gzip.open('/Users/lib-user/Library/CloudStorage/OneDrive-DalhousieUniversity/Winter 2022/Machine Learning/featuresleuth/MNISTDataset/MNIST_train_data.gz', 'r')
+        file_reader = gzip.open(LoadData.file_path, 'r')
         file_reader.read(16)
         buf = file_reader.read(28 * 28 * 1)
         train_data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-        print('demo')
+        # print('demo')
         return train_data
 
     def PrintImage(data):
-        image = im.fromarray(np.reshape(data,(28,28)))
+        image = im.fromarray(np.reshape(data, (28, 28)), mode=None)
         image.show()
 
     def __call__(self, ):
         return self.Load_Data()
+
+
+class LoadLabels:
+    # change patch as per your file location
+    label_file_path = '/Users/rohinichandrala/Documents/Personal_Docs/Dalhousie/ML/Project/featuresleuth/MNISTDataset/MNIST_train_labels.gz'
+
+    def load_labels(self):
+        train_labels = gzip.open(LoadLabels.label_file_path, 'rb')
+        buf = train_labels.read()
+        train_labels = np.frombuffer(buf, dtype=np.uint8, offset=8).astype(np.int32)
+        return train_labels
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-
 from Research.DataAnalysis import LoadData
+from Research.DataAnalysis import LoadLabels
 
 if __name__ == '__main__':
     train_data = LoadData.Load_Data()
     LoadData.PrintImage(train_data)
+    load_labels = LoadLabels()
+    train_labels = load_labels.load_labels()
+    train_label_count = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "0": 0}
+
+    for i in range(len(train_labels)):
+        count = train_label_count["{}".format(train_labels[i])]
+        train_label_count["{}".format(train_labels[i])] = count + 1
+
+    print(train_label_count)
