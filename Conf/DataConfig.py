78d47f5d7b9044883817b55ab58c65b0114ee749
Mayank <Mayank@DESKTOP-OLOR6NC>
2022-03-22 04:07:11 -0300

    Implemented loader for test data.

--- Conf/DataConfig.py
+++ Conf/DataConfig.py
@@ -4,6 +4,12 @@ from dataclasses import dataclass
 class params:
     train_data_path: str
     train_labels_path: str
+    test_data_path: str
+    test_labels_path: str
+
+@dataclass
+class hyperparams:
+    batch_size: int
 
 @dataclass
 class MNISTConfig:
