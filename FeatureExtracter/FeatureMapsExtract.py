0d5626f5ab753ff11aa67c908803b5802d971185
rohini.chandrala <rohini.chandrala@seamless.se>
2022-04-10 13:50:38 -0300

    FeatureMap Exp file changes

--- FeatureExtracter/FeatureMapsExtract.py
+++ FeatureExtracter/FeatureMapsExtract.py
@@ -7,7 +7,7 @@ from torchvision.models.feature_extraction import create_feature_extractor
 
 class FmapExtract:
 
-    def extract_featuremaps(batch_images, model):
+    def extract_featuremaps(batch_images, model, feature_extraction_layers):
         model_weights = []
         conv_layers = []
         model_children = list(model.children())
@@ -25,45 +25,38 @@ class FmapExtract:
                             model_weights.append(child.weight)
                             conv_layers.append(child)
 
-        outputs = []
-        names = []
-        for layer in conv_layers[0:1]:
-            # print(batch_images.shape)
-            image = layer(batch_images)
-            # print(image.shape)
-            outputs.append(image)
-            names.append(str(layer))
+        feature_extractor = create_feature_extractor(model, return_nodes=feature_extraction_layers)
+        feature_map_images = feature_extractor(batch_images)
 
-        # print("output length", len(outputs))
-        processed = []
-        for feature_map in outputs:
-            # print((feature_map.size()))
-            for f in range(feature_map.size()[1]):
-                processed.append(feature_map[0][f])
+        return feature_map_images
 
-        feature_map_image = []
-        for i in range(len(processed)):
-            feature_map_image.append(processed[i])
-
-        return feature_map_image
-
-    def getfeatures_from_loader(input_loader, model):
-        train_feature_maps = []
-        train_labels = []
+    def getfeatures_from_loader(input_loader, model, feature_extraction_layers, sum_up_feature_channels=True):
+        feature_maps = None
+        train_labels = None
         for images, labels in input_loader:
-            ExtractedFmaplist = FmapExtract.extract_featuremaps(images, model)
-            for fmap in range(len(ExtractedFmaplist)):
-                train_feature_maps.append(ExtractedFmaplist[fmap].detach().reshape(1, ExtractedFmaplist[fmap].size()[0], ExtractedFmaplist[fmap].size()[1]))
-                train_labels.append(labels.item())
-        # feature_list = []
-        # for i in train_feature_maps:
-        #     feature_list.append(torch.tensor(i.reshape(1, 24, 24)))
-        # print("Inside get features from loader")
-        # print(len(train_feature_maps), len(train_labels))
-        # return np.array(train_feature_maps), np.array(train_labels, dtype=np.uint8)
-        return [t.numpy() for t in train_feature_maps], np.array(train_labels, dtype=np.uint8)
+            feature_map_dict = FmapExtract.extract_featuremaps(images, model, feature_extraction_layers)
+            for layer in feature_map_dict.keys():
+                if sum_up_feature_channels:
+                    if feature_maps is None:
+                        feature_maps = torch.sum(feature_map_dict[layer], dim=1, keepdim=True)
+                        train_labels = labels
+                    else:
+                        feature_maps = torch.vstack(
+                            (feature_maps, torch.sum(feature_map_dict[layer], dim=1, keepdim=True)))
+                        train_labels = torch.cat((train_labels, labels), 0)
+                else:
+                    temp = feature_map_dict[layer]
+                    current_f = torch.reshape(temp, (len(temp) * temp.shape[1], 1, temp.shape[2], temp.shape[3]))
+                    if feature_maps is None:
+                        feature_maps = current_f
+                    else:
+                        feature_maps = torch.vstack((feature_maps, current_f))
+                    if train_labels is None:
+                        train_labels = labels.repeat_interleave(temp.shape[1])
+                    else:
+                        train_labels = torch.cat((train_labels, labels.repeat_interleave(temp.shape[1])))
+        return [t.detach().numpy() for t in feature_maps], np.array(train_labels, dtype=np.uint8)
 
-    
     def get_feature_maps(model, batch_images):
         feature_extractor = create_feature_extractor(model, return_nodes=['conv1'])
         newBatchFeatureMaps = feature_extractor(batch_images)
@@ -72,7 +65,7 @@ class FmapExtract:
         Z = torch.sum(newBatchFeatureMaps['conv1'].flatten(start_dim=2, end_dim=3), dim=1)
         # print(type(Z.shape))
         return Z
-    
+
     def extract_featuremaps_new(batch_images, model):
         model_weights = []
         conv_layers = []
